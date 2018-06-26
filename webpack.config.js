3a896c5af8bea44215a7ce970ba8d687b0fec5fc
Rahul Patil <rp7187655@gmail.com>
2018-06-26 22:12:54 +0530

    Add setup to start the react app

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+module.exports = {
+    mode: 'none',
+    entry: __dirname + '/src/index.js',
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output:{
+        filename: 'transformed.js',
+        path: __dirname + '/build'
+    },
+    devServer:{
+        port: 5000,
+        publicPath: 'http://localhost:5000'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
