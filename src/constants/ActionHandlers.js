6badc198d4ffa1aa84508d28b3eb8f2a7df5a2d3
Praveen Mathew <mpraveen@thoughtworks.com>
2018-07-04 23:22:08 +0530

    reducer to handle name reload

--- src/constants/ActionHandlers.js
+++ src/constants/ActionHandlers.js
@@ -1,9 +1,16 @@
+import nameList from './../NameList';
+
 const remove = (state, action) => ({
     name: state.name.filter(name => !action.name.includes(name))
 })
 
+const reload = (state, action) =>({
+    ...nameList
+});
+
 const actionHandler = {
-    'REMOVE': remove
+    'REMOVE': remove,
+    'RELOAD': reload
 };
 
 export default actionHandler;
--- test/reducers/StandUp.spec.js
+++ test/reducers/StandUp.spec.js
@@ -1,10 +1,15 @@
 import * as actions from './../../src/actions/StandUp';
 import createReducer from '../../src/reducers/createReducer';
+import nameList from './../../src/NameList'
 
 describe('Stand up reducer', () => {
     it('should remove the name after spin', () => {
         const initialState = {name: ['foo', 'bar', 'goo', 'dud']};
         const expectedState = {name: ['foo', 'bar']};
         expect(createReducer(null, null)(initialState, actions.remove(['goo', 'dud']))).toEqual(expectedState);
+    });
+
+    it('should return the initial name list on reload', () => {
+        expect(createReducer()(null, actions.reload())).toEqual(nameList);
     });
 });
