3f032e6222a851063926972e25a95378244279b7
Praveen Mathew <mpraveen@thoughtworks.com>
2018-07-09 02:17:55 +0530

    added css

--- src/components/StandUp.js
+++ src/components/StandUp.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import './../css/Standup.css';
 
 const display = (name) =>(
     name?
-        name[0] + "     " + name[1]:
+        name.map((item, i) => (
+        <p key={i}>{item}</p>)):
     location.reload()
 )
 
 const TWENTY_FOUR_HOURS = 1000 * 60 * 60 * 24;
 
 const next = change => {
-    setInterval(change, 1500)
+    setInterval(change, TWENTY_FOUR_HOURS)
 }
 
 const Standup = (props) => {
     return (
     <div className="StandUp">
         {display(props.name)}
-        <br />
         {next(() => (props.spin(props.name)))}
         <button onClick={() => (props.spin(props.name))}>
             SPIN
